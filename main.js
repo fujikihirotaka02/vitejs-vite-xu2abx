cc04671183c7e3f388892d04ed408c7e57c407e3
null <null>
2024-08-22 10:26:49 +0900

    var変数完了、let変数の途中

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('hoge');
+/* 
+comment
+*/
+
+// var val1 = "var変数";
+// console.log(val1);
+
+// //var 変数なので値を上書きできる。
+// val1 = "uwagaki"
+// console.log(val1);
+
+// //再宣言可能な変数
+// var val1 = "再宣言"
+// console.log(val1);
+
+let val2 = "let変数";
+console.log(val2);
+
+val2 = "let変数上書き";
+console.log(val2);
